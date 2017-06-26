14a663580a0c379019b0cf2232bed56c563cca5b
mariusmue <marius.muench@eurecom.fr>
2017-06-26 12:59:20 +0200

    removed response-listener from qmp-protocol

--- avatar2/protocols/qmp.py
+++ avatar2/protocols/qmp.py
@@ -4,70 +4,10 @@ if sys.version_info < (3, 0):
 else:
     import queue
 
-from threading import Thread, Event, Condition
 import logging
 import json
 import telnetlib
 import re
-
-
-class QMPResponseListener(Thread):
-
-    def __init__(self, gdb_protocol, gdb_controller, avatar_queue, origin=None):
-        super(QMPResponseListener, self).__init__()
-        self._protocol = gdb_protocol
-        self._token = -1
-        self._async_responses = queue.Queue() if avatar_queue is None\
-            else avatar_queue
-        self._sync_responses = {}
-        self._gdb_controller = gdb_controller
-        self._gdb = gdb_protocol
-        self._close = Event()
-        self._closed = Event()
-        self._close.clear()
-        self._closed.clear()
-        self._sync_responses_cv = Condition()
-        self._last_exec_token = 0
-        self._origin = origin
-        self.log = logging.getLogger('%s.%s' %
-                                     (origin.log.name, self.__class__.__name__)
-                                    ) if origin else \
-                                     logging.getLogger(self.__class__.__name__)
-    
-    def run(self):
-        while(1):
-            if self._close.is_set():
-                break
-
-            responses = None
-
-            try:
-                responses = self._gdb_controller.get_gdb_response(
-                    timeout_sec=0.5
-                )
-            except:
-                continue
-
-            for response in responses:
-                if response.get('token', None) is not None:
-                    self._sync_responses_cv.acquire()
-                    self._sync_responses[response['token']] = response
-                    self._sync_responses_cv.notifyAll()
-                    self._sync_responses_cv.release()
-                else:
-                    avatar_msg = self.parse_async_response(response)
-                    self.log.debug("Parsed an avatar_msg %s", avatar_msg)
-                    if avatar_msg is not None:
-                        if self._gdb._async_message_handler is not None:
-                            self._gdb._async_message_handler(avatar_msg)
-                        else:
-                            self._async_responses.put(avatar_msg)
-        self._closed.set()
-
-    def stop(self):
-        """Stops the listening thread. Useful for teardown of the target"""
-        self._close.set()
-        self._closed.wait()
 
 
 class QMPProtocol(object):
@@ -81,9 +21,6 @@ class QMPProtocol(object):
                                      logging.getLogger(self.__class__.__name__)
         self.id = 0
 
-        #self._communicator = QMPResponseListener(self, origin.avatar.queue, 
-                                                 #origin)
-        #self._communicator.start()
 
     def __del__(self):
         self.shutdown()
