2600abc97425493744e1590377b9819fc6da26a1
Eric Gustafson <edg@cs.ucsb.edu>
2017-09-20 17:10:20 -0700

    Move to the OpenOCD TCL machine port

--- avatar2/protocols/openocd.py
+++ avatar2/protocols/openocd.py
@@ -13,27 +13,24 @@ if sys.version_info < (3, 0):
 else:
     import queue
 
-END_OF_MSG = b'\r\n\r>'
+END_OF_MSG = b'\x1a'
 
 
 class OpenOCDProtocol(Thread):
     """
     This class implements the openocd protocol.
-    Although OpenOCD itself is very powerful, it is only used as monitor
-    protocol, since all other functionalities are also exposed via the 
-    gdb-interface, which is easier to parse in an automatic manner.
 
     :param openocd_script:     The openocd scripts to be executed.
     :type openocd_script:      str or list
     :param openocd_executable: The executable
     :param additional_args:    Additional arguments delivered to openocd.
     :type  additional_args:    list
-    :param telnet_port:        the port used for the telnet connection
+    :param tcl_port:        the port used for the telnet connection
     :param gdb_port:           the port used for openocds gdb-server
     """
 
     def __init__(self, avatar, openocd_script, openocd_executable="openocd",
-                 additional_args=[], host='127.0.0.1', telnet_port=4444, gdb_port=3333,
+                 additional_args=[], host='127.0.0.1', tcl_port=6666, gdb_port=3333,
                  origin=None, output_directory='/tmp'):
         if isinstance(openocd_script, str):
             self.openocd_files = [openocd_script]
@@ -41,11 +38,15 @@ class OpenOCDProtocol(Thread):
             self.openocd_files = openocd_script
         else:
             raise TypeError("Wrong type for OpenOCD configuration files")
-
-        self._telnet_port = telnet_port
+        self.log = logging.getLogger('%s.%s' % (origin.log.name, self.__class__.__name__)) if origin else \
+                                    logging.getLogger(self.__class__.__name__)
+        self._tcl_port = tcl_port
+        self._gdb_port = gdb_port
         self._host = host
         self.in_queue = queue.Queue()
         self.out_queue = queue.Queue()
+        self.trace_queue = queue.Queue()
+        self.trace_enabled = Event()
         self.avatar = avatar
         self.telnet = None
         self._close = Event()
@@ -53,42 +54,60 @@ class OpenOCDProtocol(Thread):
         self.cmd_lock = Lock()
 
         executable_path = distutils.spawn.find_executable(openocd_executable)
-
-        self._cmd_line = [executable_path ,
-                          '--command', 'telnet_port %d' % telnet_port,
-                          '--command', 'gdb_port %d' % gdb_port]
-        self._cmd_line += additional_args
+        self._cmd_line = [executable_path, '--debug']
         self._cmd_line += [e for l
                            in [['-f', abspath(f)] for f in self.openocd_files]
                            for e in l]
+        self._cmd_line += ['--command', 'tcl_port %d' % self._tcl_port,
+                          '--command', 'gdb_port %d' % self._gdb_port]
+        self._cmd_line += additional_args
 
         self._openocd = None
 
         with open("%s/openocd_out.txt" % output_directory, "wb") as out, \
                 open("%s/openocd_err.txt" % output_directory, "wb") as err:
+            self.log.debug("Starting OpenOCD with command line: %s" % (" ".join(self._cmd_line)))
             self._openocd = subprocess.Popen(self._cmd_line,
                                              stdout=out, stderr=err)#, shell=True)
-        self.log = logging.getLogger('%s.%s' %
-                                     (origin.log.name, self.__class__.__name__)
-                                     ) if origin else \
-            logging.getLogger(self.__class__.__name__)
         Thread.__init__(self)
+
 
     def connect(self):
         """
-        Connects to OpenOCDs telnet-server for all subsequent communication
+        Connects to OpenOCDs TCL Server for all subsequent communication
         returns: True on success, else False
         """
         from time import sleep
         sleep(1)
-        self.log.debug("Connecting to OpenOCD on %s:%s" % (self._host, self._telnet_port))
-        self.telnet = telnetlib.Telnet(self._host, self._telnet_port)
-        resp = self.telnet.read_until(END_OF_MSG)
-        if 'Open On-Chip Debugger' in str(resp):
+        self.log.debug("Connecting to OpenOCD on %s:%s" % (self._host, self._tcl_port))
+        try:
+            self.telnet = telnetlib.Telnet(self._host, self._tcl_port)
+            # mic check
+            self.telnet.write("ocd_echo\x1a")
+            self.log.debug("Connected to OpenOCD.  Saying hello...")
+            stuff = self.telnet.read_until('\x1a')
+            self.log.debug("Got a hello back.  Starting background thread...")
             self.start()
             return True
+        except:
+            self.log.exception("Error connecting to OpenOCD TCL port %d" % self._tcl_port)
+            return False
+
+    def enable_trace(self):
+        """
+        Enables OpenOCD's "target tracing" support.
+        Requires a version of OpenOCD >= 0.10.0-dev
+        (the absolute latest one is probably a good idea)
+        This will log all the trace data to the special `trace_queue` member.
+        Something else (like the CoreSightProtocol) should figure out what to do with that.
+        :return:
+        """
+        self.log.debug("Enabling tracing...")
+        resp = self.execute_command("ocd_tcl_trace on")
+        if 'is enabled' in resp:
+            return True
         else:
-            self.log.error('Failed to connect to OpenOCD')
+            self.log.warning("Could not enable target tracing! Is your OpenOCD old?")
             return False
 
     def reset(self):
@@ -96,7 +115,8 @@ class OpenOCDProtocol(Thread):
         Resets the target
         returns: True on success, else False
         """
-        resp = self.execute_command('reset halt', recv_response=True)
+        self.log.debug("Resetting target")
+        resp = self.execute_command('reset halt')
         if not 'Not halted' in str(resp):
             self.log.debug("Target reset complete")
             return True
@@ -116,24 +136,11 @@ class OpenOCDProtocol(Thread):
             self._openocd.terminate()
             self._openocd = None
 
-    def execute_command(self, cmd, recv_response=False, wait_for_re=None):
+    def execute_command(self, cmd):
         try:
             self.cmd_lock.acquire()
             self.in_queue.put(cmd)
-            while not self.avatar._close.is_set() and not self._close.is_set():
-                new_line = self.out_queue.get()
-                """
-                if cmd in new_line:
-                    # Kill the echo
-                    if not recv_response:
-                        return
-                    else:
-                        continue
-                """
-                if not recv_response:
-                    return
-                else:
-                    return new_line
+            return self.out_queue.get()
         except Exception, e:
             self.log.exception("Exception thrown executing command")
             raise e
@@ -141,30 +148,34 @@ class OpenOCDProtocol(Thread):
             self.cmd_lock.release()
 
     def run(self):
-        self.log.debug("Starting OpenOCDSocketListener")
-        while not self.avatar._close.is_set() and not self._close.is_set():
-            if not self.in_queue.empty():
-                cmd = self.in_queue.get()
-                self.telnet.write((cmd + '\n').encode('ascii'))
-            try:
-                line = self.read_response()
-            except EOFError:
-                self.log.warning("OpenOCD Connection closed!")
-                self.shutdown()
-                break
-            if line:
-                if "WARNING" in line:
-                    self.log.warning(line)
-                else:
-                    self.log.debug(line)
-                    self.out_queue.put(line)
+        try:
+            self.log.debug("Starting OpenOCDSocketListener")
+            while not self.avatar._close.is_set() and not self._close.is_set():
+                if not self.in_queue.empty():
+                    cmd = self.in_queue.get()
+                    self.log.debug("Executing command %s" % cmd)
+                    self.telnet.write((cmd + END_OF_MSG))
+                try:
+                    line = self.read_response()
+                except EOFError:
+                    self.log.warning("OpenOCD Connection closed!")
+                    self.shutdown()
+                    break
+                if line is not None:
+                    line = line.rstrip(END_OF_MSG)
+                    if "trace_data" in line:
+                        self.trace_queue.put(line)
+                    else:
+                        self.log.debug(line)
+                        self.out_queue.put(line)
+        except Exception, e:
+            self.log.exception("OpenOCD Background thread died with an exception")
+        self.log.debug("OpenOCD Background thread exiting")
 
     def read_response(self):
         self.buf += self.telnet.read_eager()
         if END_OF_MSG in self.buf:
-            idx = self.buf.index(END_OF_MSG) + len(END_OF_MSG)
-            resp = self.buf[:idx]
-            self.buf = self.buf[idx:]
+            resp, self.buf = self.buf.split(END_OF_MSG, 1)
             return resp
         return None
 
@@ -193,7 +204,7 @@ class OpenOCDProtocol(Thread):
                 # A list of ints
                 write_val = hex(val[i]).rstrip("L")
             write_addr = hex(address + i).rstrip("L")
-            self.execute_command('mww %s %s' % (write_addr, write_val), recv_response=True)
+            self.execute_command('mww %s %s' % (write_addr, write_val))
 
         return True
 
@@ -211,9 +222,9 @@ class OpenOCDProtocol(Thread):
         words = []
         for i in range(0, num_words, wordsize):
             read_addr = hex(address + i).rstrip('L')
-            resp = self.execute_command('mrw %s' % read_addr, recv_response=True)
+            resp = self.execute_command('mrw %s' % read_addr)
             if resp:
-                val = int(resp.splitlines()[1])
+                val = int(resp)
                 raw_mem += binascii.unhexlify(hex(val)[2:].zfill(wordsize * 2))
             else:
                 self.log.error("Could not read from address %s" % read_addr)
--- avatar2/targets/openocd_target.py
+++ avatar2/targets/openocd_target.py
@@ -5,7 +5,7 @@ if sys.version_info < (3, 0):
 else:
     from queue import PriorityQueue
 import time
-
+import os
 from avatar2.targets import Target
 from avatar2.protocols.gdb import GDBProtocol
 from avatar2.protocols.openocd import OpenOCDProtocol
@@ -14,11 +14,13 @@ from avatar2.protocols.openocd import OpenOCDProtocol
 class OpenOCDTarget(Target):
     def __init__(self, avatar, executable=None,
                  openocd_script=None, additional_args=None,
-                 telnet_port=4444,
+                 tcl_port=6666,
                  gdb_executable=None, gdb_additional_args=None, gdb_port=3333,
                  **kwargs
                  ):
 
+        if openocd_script and not os.path.exists(openocd_script):
+            raise ValueError("OpenOCD script %s does not exist!" % openocd_script)
         super(OpenOCDTarget, self).__init__(avatar, **kwargs)
 
         self.executable = (executable if executable is not None
@@ -26,10 +28,10 @@ class OpenOCDTarget(Target):
         self.avatar = avatar
         self.openocd_script = openocd_script
         self.additional_args = additional_args if additional_args else []
-        self.telnet_port = telnet_port
 
         self.gdb_executable = (gdb_executable if gdb_executable is not None
                                else self._arch.get_gdb_executable())
+        self.tcl_port = tcl_port
         self.gdb_additional_args = gdb_additional_args if gdb_additional_args else []
         self.gdb_port = gdb_port
 
@@ -37,7 +39,7 @@ class OpenOCDTarget(Target):
         openocd = OpenOCDProtocol(self.avatar, self.openocd_script,
                                   openocd_executable=self.executable,
                                   additional_args=self.additional_args,
-                                  telnet_port=self.telnet_port,
+                                  tcl_port=self.tcl_port,
                                   gdb_port=self.gdb_port,
                                   origin=self,
                                   output_directory=self.avatar.output_directory)
@@ -48,12 +50,24 @@ class OpenOCDTarget(Target):
                           avatar=self.avatar, origin=self)
 
         time.sleep(.1)  # give openocd time to start. Find a better solution?
-
-        if openocd.connect() and gdb.remote_connect(port=self.gdb_port):
-            openocd.reset()
-            self.log.info("Connected to Target")
+        self.log.debug("Connecting to OpenOCD telnet port")
+        ocd_connected = openocd.connect()
+        self.log.debug("Connecting to OpenOCD GDB port")
+        gdb_connected = gdb.remote_connect(port=self.gdb_port)
+        script_has_reset = False
+        if self.openocd_script:
+            with open(self.openocd_script) as f:
+                script = f.read()
+            if "reset halt" in script:
+                self.log.debug("Not resetting target, script may have done it already")
+                script_has_reset = True
+        if ocd_connected and gdb_connected:
+            self.log.info("Successfully connected to OpenOCD target!")
         else:
-            self.log.warning("Connecting failed")
+            self.log.error("Failed to connect to OpenOCD target!")
+        if ocd_connected and gdb_connected and not script_has_reset:
+            self.log.debug("Resetting target...")
+            openocd.reset()
 
         self.protocols.set_all(gdb)
         self.protocols.memory = openocd
