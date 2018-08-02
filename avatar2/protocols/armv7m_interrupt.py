bf5e80afcc6963826c01b42526f981e5511dbea9
mariusmue <marius.muench@eurecom.fr>
2018-08-02 11:28:44 -0700

    set vector table base

--- avatar2/protocols/armv7m_interrupt.py
+++ avatar2/protocols/armv7m_interrupt.py
@@ -168,6 +168,13 @@ class ARMV7MInterruptProtocol(Thread):
                  {'num_irq': interrupt_number, 'num_cpu': cpu_number}
             )
 
+    def set_vector_table_base(self, base, cpu_number=0):
+        if isinstance(self._origin, QemuTarget):
+            self.log.info("Setting vector table base to 0x%x" % base)
+            self._origin.protocols.monitor.execute_command(
+                'avatar-armv7m-set-vector-table-base',
+                 {'base': base, 'num_cpu': cpu_number}
+            )
     def send_interrupt_exit_response(self, id, success):
         response =  V7MInterruptNotificationAck(id, success,
                                                 RINOperation.EXIT.value)
