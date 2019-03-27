2a382d857914dad328dbfbc159512514ac2e3cd5
ChrisMcMStone <christopher-ms@hotmail.com>
2019-03-27 16:11:21 +0100

    Added some basic avatar test script for testing angr integration

---
+++ tests/test_avatar-angr.py
@@ -0,0 +1,32 @@
+import subprocess
+import os
+import avatar2 as avatar2
+import logging
+
+GDB_SERVER_IP = "127.0.0.1"
+GDB_SERVER_PORT = "2222"
+binary_path = "/home/chris/Documents/phd/hw_symb/angr/"
+binary = binary_path + "fauxware"
+bp_func = "print_test"
+
+ava = avatar2.Avatar(arch=avatar2.archs.X86_64,output_directory="/home/chris/Documents/phd/hw_symb/angr")
+ava.load_plugin("gdb_memory_map_loader")
+# Set log level
+ava.log.setLevel(logging.DEBUG)
+print("[+] Creating the GDBTarget")
+gdb = ava.add_target(avatar2.GDBTarget, gdb_port=GDB_SERVER_PORT, local_binary=binary)
+
+print("[+] Initializing the targets")
+ava.init_targets()
+
+gdb.disable_aslr()
+
+print("[+] Running binary until breakpoint")
+gdb.bp(bp_func)
+
+# Equivalent to run, but only if not already running
+gdb.cont()
+gdb.wait()
+
+# Insert angr memory tranfer code here...
+
