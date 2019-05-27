7fc12085f27b6489ad6397c4381716589c88eb43
mariusmue <marius.muench@eurecom.fr>
2019-05-27 21:03:03 +0200

    Improvements for monitor register retrival
    * Check for the architecture during retrieval and error if not supported
    * Implementation of x86 specific `x86_get_segment_register_base` function

--- avatar2/protocols/qmp.py
+++ avatar2/protocols/qmp.py
@@ -10,6 +10,8 @@ import json
 import telnetlib
 import re
 
+from avatar2 import archs
+
 
 class QMPProtocol(object):
     def __init__(self, port, origin=None):
@@ -19,6 +21,7 @@ class QMPProtocol(object):
                                      (origin.log.name, self.__class__.__name__)
                                      ) if origin else \
             logging.getLogger(self.__class__.__name__)
+        self.origin=origin
         self.id = 0
 
     def __del__(self):
@@ -68,14 +71,40 @@ class QMPProtocol(object):
         # self._communicator.stop()
         pass
 
-    def get_registers(self):
+    def get_registers(self, raw=False):
         """
         Gets the current register state based on the hmp info registers
         command. In comparison to register-access with the register protocol,
         this function can also be called while the target is executing.
-        returns: A dictionary with the registers
+        :param raw:     If true, return output of the hmp-cmd without parsing
+        :type raw:      ``bool``
+        :return:        A dictionary with the registers or the register-string
         """
+
         regs_s = self.execute_command("human-monitor-command",
                                       {"command-line": "info registers"})
-        regs_r = re.findall('(...)=([0-9a-f]{8})', regs_s)
-        return dict([(r.lower(), int(v, 16)) for r, v in regs_r])
+        if raw is True:
+            return regs_s
+
+        if issubclass(self.origin.avatar.arch, archs.ARM):
+            regs_r = re.findall('(...)=([0-9a-f]{8})', regs_s)
+            return dict([(r.lower(), int(v, 16)) for r, v in regs_r])
+        else:
+            raise Exception( ("get_registers in non-raw mode called on "
+                              "unsupported arch %s") %
+                              self.origin.avatar.arch.__name__)
+
+    def x86_get_segment_register_base(self, reg):
+        """
+        x86-only: get the segment register base using the hmp.
+        :param reg:     name of the register
+        :type reg:      ``str``
+        :return:        The base of segment register ``reg`` on success
+        """
+
+        regs = self.get_registers(raw=True)
+        regs_r = re.findall(reg.upper()+'\s?=[0-9a-f]+\s([0-9a-f]+)', regs)
+        if len(regs_r) != 1:
+            raise exception("Couldn't retrieve base for %s" % reg)
+
+        return int(regs_r[0], 16)
