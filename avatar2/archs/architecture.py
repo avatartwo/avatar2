18ae3985c3f63624a04308eaeab5af2ab2055df0
mariusmue <m.muench@vu.nl>
2020-01-21 16:18:26 +0100

    allow setting of target executable via env

--- avatar2/archs/architecture.py
+++ avatar2/archs/architecture.py
@@ -1,4 +1,7 @@
 from avatar2.installer.config import AvatarConfig
+import distutils
+from os import environ
+from re import sub
 
 class Architecture(object):
     
@@ -10,11 +13,20 @@ class Architecture(object):
         Convienently enough, this config already checks whether the executable
         is installed on the system
         """
-        target_path = AvatarConfig().get_target_path(exec_name)
+
+        env_var_name = 'AVATAR2_%s_EXECUTABLE' % sub(r'avatar-|\W\(.*\)',
+                                                     '', exec_name).upper()
+        env_exec = environ.get( env_var_name )
+        if env_exec is not None:
+            target_path = distutils.spawn.find_executable(env_exec)
+        else:
+            target_path = AvatarConfig().get_target_path(exec_name)
         if target_path is None:
             raise Exception(("Couldn't find executable for %s\n"
                              "Have you tried running the avatar2-installer "
-                             "(python -m avatar2.installer)?" % exec_name))
+                             "(python -m avatar2.installer) or setting the %s "
+                             "environment variable?" % (exec_name, env_var_name)
+                            ))
         return target_path
 
     @staticmethod
