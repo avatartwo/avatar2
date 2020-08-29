9a2b6381256a994b7f8a52119564534465889501
Ruud Linssen <ruudjelinssen21@gmail.com>
2020-08-29 11:19:11 +0200

    Fixes python 3.5 not supported in pygdbmi 0.10.0.0

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ setup(
               'avatar2/installer'
               ],
     install_requires=[
-        'pygdbmi==0.9.0.2;python_version<"3.5"',
-        'pygdbmi>=0.10.0.0;python_version>="3.5"',
+        'pygdbmi==0.9.0.2;python_version<="3.5"',
+        'pygdbmi>=0.10.0.0;python_version>"3.5"',
         'intervaltree',
         'posix_ipc>=1.0.0',
         'capstone>=3.0.4',
