e13f6767f63129f5604b029ee23f0182ea8d7839
mariusmue <m.muench@vu.nl>
2020-12-09 10:19:53 +0100

    use modern pypanda api

--- avatar2/targets/pypanda_target.py
+++ avatar2/targets/pypanda_target.py
@@ -10,7 +10,7 @@ class PyPandaTarget(PandaTarget):
     '''
     def __init__(self, *args, **kwargs):
         try:
-            import panda
+            import pandare
         except ImportError:
             raise RuntimeError(("PyPanda could not be found! for installation, "
                     "please follow the steps at https://github.com/"
@@ -20,18 +20,18 @@ class PyPandaTarget(PandaTarget):
 
         self.pypanda = None
         self._thread = None
-    
+
     def shutdown(self):
         if self._thread.is_alive():
             self.pypanda.end_analysis()
 
 
     def init(self, **kwargs):
-        from panda import Panda
+        from pandare import Panda
 
         arch = self.avatar.arch.gdb_name # for now, gdbname and panda-name match
         args = self.assemble_cmd_line()[1:]
-        
+
 
 
         self.avatar.save_config(file_name=self.qemu_config_file,
@@ -39,9 +39,9 @@ class PyPandaTarget(PandaTarget):
 
 
         self.pypanda = Panda(arch=arch, extra_args=args, **kwargs)
-        
+
         # pypanda.run() is blocking, hence we run it in a seperate thread
         self._thread = Thread(target=self.pypanda.run, daemon=True)
         self._thread.start()
-        
+
         self._connect_protocols()
