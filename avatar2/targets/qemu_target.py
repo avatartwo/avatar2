5271aedfec5da018a2f7a532449d4edea5889010
rawsample <paul.olivier@eurecom.fr>
2023-12-01 17:25:23 +0100

    Remove crashing log line when using pypanda

--- avatar2/targets/qemu_target.py
+++ avatar2/targets/qemu_target.py
@@ -80,7 +80,6 @@ class QemuTarget(Target):
             self.avatar.output_directory,
             self.name,
         )
-        self.log.info("QEmuTarget using executable '%s'" % self.executable)
 
     def assemble_cmd_line(self):
         if isfile(self.executable + self._arch.qemu_name):
